3ea291e376f12dd74b7562c7af1285b21996be43
DarioOjeda <darioojeda_1995@hotmail.com>
2021-07-18 19:50:49 +0200

    estructura tipica de rutas hijas a traves del home component de heroes

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -4,10 +4,12 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'listado',
@@ -18,7 +20,7 @@ const routes: Routes = [
         component: AgregarComponent
       },
       {
-        path: 'editar',
+        path: 'editar/:id',
         component: AgregarComponent
       },
       {
@@ -41,6 +43,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class HeroesRoutingModule { }
