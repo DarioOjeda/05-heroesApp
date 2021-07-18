edb130fe66f129751a536d34ba165f8c3455d813
DarioOjeda <darioojeda_1995@hotmail.com>
2021-07-18 18:15:02 +0200

    estructura inicial de modulos y rutas
    preparada para hacer lazy load

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes } from "@angular/router";
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+
+const routes: Routes = [
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent,
+  }
+]
+
+@NgModule({
+  imports: [
+    
+  ]
+})
+export class AppRoutingModule { }
